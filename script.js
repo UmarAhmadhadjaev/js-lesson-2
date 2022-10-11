3c3b0ccf8d96a1f0c2dec492039f7eb002204e90
UmarAhmadxadjaev <azizhonahmadhadjaev@gmail.com>
2022-10-12 04:50:39 +0500

    js lesson-2

---
+++ script.js
@@ -0,0 +1,63 @@
+// 1-masala;
+
+// let X = prompt ("Raqam kiriting sm da");
+// let Y =X/100;
+// console.log(Y +" metr") ;
+ 
+
+// 2-masala;
+
+// let X = prompt ("120");
+// let Y =X/1000;
+// console.log(Y + "tonna");
+ 
+
+// 3-masala;
+
+// let A = prompt ("Birinchi sonni kiriting");
+// let B = prompt ("Ikkinchi sonni kiriting");
+// let C = A/B;
+// console.log(C+ " marta joylashtirish mumkin")
+
+
+// 4-masala;
+
+// let xy=prompt("Ikki xonali son kiriting");
+// let x=Math.floor(xy/10);
+// let y=xy%10;
+// console.log(x+ " ta o'nliklar")
+// console.log(y+ " ta birliklar")
+
+ 
+// 5-masala;
+
+// let x=6;
+// let y=4;
+// let z=x+y;
+// console.log(z+" ikki xonali sonni raqamlarini yig'indisi")
+
+
+// 6-masala;
+
+// let x=4;
+// let y=4;
+// let z=x*y;
+// console.log(z+" ikki xonali sonni raqamlarini ko'paytmasi")
+
+
+// 7-masala;
+
+// let A=prompt("999 dan kotta son kiritng!");
+// let B=Math.floor(A/100);
+// let C=A%100;
+// console.log(B+" ta yuzliklar")
+// console.log(C+' qoldiq')
+
+
+// 8-masala;
+
+// let A=prompt("1200");
+// let B=A/60;
+// let C=A/3600;
+// console.log(B+" minut o'tdi")
+// console.log(C+" soat o'tdi")
